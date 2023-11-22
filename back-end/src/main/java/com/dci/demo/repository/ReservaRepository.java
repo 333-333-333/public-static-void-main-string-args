fe95391d7d4814925e351adad0a11e90bb525a62
333-333-333 <davidemanuelms@gmail.com>
2023-11-22 00:48:50 -0300

    add: Repositorio, servicio y controlador de Reservas actualizados con funcionalidades para buscar reservas por establecimiento y date-time en adelante.

--- back-end/src/main/java/com/dci/demo/repository/ReservaRepository.java
+++ back-end/src/main/java/com/dci/demo/repository/ReservaRepository.java
@@ -2,8 +2,22 @@ package com.dci.demo.repository;
 
 import com.dci.demo.domain.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+
+    @Query("SELECT r FROM Reserva r WHERE r.resRecinto.recId = :rec_Id")
+    List<Reserva> findReservasByRecinto(@Param("rec_Id") Long recId);
+
+    // Reservas por recinto y desde el dia y hora en cuestión en adelante.
+    @Query("SELECT r FROM Reserva r WHERE r.resRecinto.recId = :rec_Id AND r.resInicio >= :res_Inicio")
+    List<Reserva> findReservasByRecintoAndInicio(@Param("rec_Id") Long recId, @Param("res_Inicio") Date resInicio);
+
+
 }
