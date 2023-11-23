e18bbde9ebad5d9dc1206ea198cf3f90b1b2e7db
Marcia Gatica <m.gatica05@ufromail.cl>
2023-11-23 04:25:05 -0300

    arreglando el admin

--- front-end/deporte-front/src/views/CrearRecinto.vue
+++ front-end/deporte-front/src/views/CrearRecinto.vue
@@ -1,24 +1,70 @@
 <!-- CrearRecinto.vue -->
 <template>
-    <div>
-      <MenuLateral />
-      <h2>Crear Recinto</h2>
-      <!-- Contenido del formulario para crear recinto -->
-      <!-- Puedes agregar aquí el contenido del formulario según tus necesidades -->
+  <div class="contenedor-principal">
+    <menu-admin /> <!-- Agrega el componente MenuAdmin.vue aquí -->
+
+    <div class="formulario">
+      <h1>Crear Recinto</h1>
+      <form @submit.prevent="crearRecinto">
+        <!-- Campos del formulario -->
+        <label for="nombre">Nombre del Recinto:</label>
+        <input type="text" v-model="nombre" required />
+
+        <label for="direccion">Dirección:</label>
+        <input type="text" v-model="direccion" required />
+
+        <label for="capacidad">Capacidad:</label>
+        <input type="number" v-model="capacidad" required />
+
+        <button type="submit">Crear Recinto</button>
+      </form>
     </div>
-  </template>
-  
-  <script>
-  import MenuLateral from "@/components/MenuAdmin.vue"; // Asegúrate de que la ruta sea correcta
-  
-  export default {
-    components: {
-      MenuLateral,
+  </div>
+</template>
+
+<script>
+import MenuAdmin from "@/components/MenuAdmin.vue"; // Asegúrate de que la ruta sea correcta
+
+export default {
+  components: {
+    MenuAdmin, // Registra el componente MenuAdmin.vue aquí
+  },
+  data() {
+    return {
+      nombre: "",
+      direccion: "",
+      capacidad: 0,
+    };
+  },
+  methods: {
+    crearRecinto() {
+      // Lógica para crear el recinto y enviar datos al servidor
+      console.log("Recinto creado:", {
+        nombre: this.nombre,
+        direccion: this.direccion,
+        capacidad: this.capacidad,
+      });
     },
-  };
-  </script>
-  
-  <style scoped>
-  /* Estilos específicos para esta vista si es necesario */
-  </style>
-  
+  },
+};
+</script>
+
+<style scoped>
+.contenedor-principal {
+  display: flex;
+}
+
+.menu-admin {
+  width: 30%;
+}
+
+.formulario {
+  margin-left: 30%;
+  width: 70%;
+  padding: 20px;
+}
+h1{
+  text-align: center;
+}
+/* Agrega estilos específicos para este componente si es necesario */
+</style>
